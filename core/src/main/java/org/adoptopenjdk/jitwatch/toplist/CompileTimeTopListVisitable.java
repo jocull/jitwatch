237ed56e516ca4a189465312cf2b8fa4918eab88
James O'Cull <james.ocull@genesys.com>
2018-08-30 10:52:07 -0400

    Added extra code to report sum of compilation times on stdout

--- core/src/main/java/org/adoptopenjdk/jitwatch/toplist/CompileTimeTopListVisitable.java
+++ core/src/main/java/org/adoptopenjdk/jitwatch/toplist/CompileTimeTopListVisitable.java
@@ -5,9 +5,10 @@
  */
 package org.adoptopenjdk.jitwatch.toplist;
 
-import org.adoptopenjdk.jitwatch.model.Compilation;
-import org.adoptopenjdk.jitwatch.model.IMetaMember;
-import org.adoptopenjdk.jitwatch.model.IReadOnlyJITDataModel;
+import org.adoptopenjdk.jitwatch.model.*;
+import org.adoptopenjdk.jitwatch.treevisitor.ITreeVisitable;
+
+import java.util.List;
 
 public class CompileTimeTopListVisitable extends AbstractTopListVisitable
 {
@@ -19,12 +20,78 @@ public class CompileTimeTopListVisitable extends AbstractTopListVisitable
 	@Override
 	public void visit(IMetaMember mm)
 	{
+		// TODO: Testing duration sums only! Revert me!
+		long durations = 0;
+		long durationsStrict = 0;
 		for (Compilation compilation : mm.getCompilations())
 		{
+			durations += compilation.getCompilationDuration();
 			if (!compilation.isC2N())
 			{
 				topList.add(new MemberScore(mm, compilation.getCompilationDuration()));
+				durationsStrict += compilation.getCompilationDuration();
 			}
-		}	
+		}
+
+		if (durations > 0 && durationsStrict <= 0) {
+			System.out.println(":::::::DIFFERENCE:::::");
+			System.out.println("Durations: " + durations);
+			System.out.println("Durations strict: " + durationsStrict);
+		}
+	}
+
+	// TODO: Duplicated code from `TreeVisitor`!
+	private void walkTree(IReadOnlyJITDataModel model)
+	{
+		List<MetaPackage> roots = model.getPackageManager().getRootPackages();
+
+		for (MetaPackage mp : roots)
+		{
+			walkPackage(mp);
+		}
+	}
+
+	// TODO: Duplicated code from `TreeVisitor`!
+	private void walkPackage(MetaPackage mp)
+	{
+		List<MetaPackage> childPackages = mp.getChildPackages();
+
+		for (MetaPackage childPackage : childPackages)
+		{
+			walkPackage(childPackage);
+		}
+
+		List<MetaClass> packageClasses = mp.getPackageClasses();
+
+		for (MetaClass mc : packageClasses)
+		{
+			for (IMetaMember mm : mc.getMetaMembers())
+			{
+				for (Compilation compilation : mm.getCompilations())
+				{
+					_durations += compilation.getCompilationDuration();
+					if (!compilation.isC2N())
+					{
+						_durationsStrict += compilation.getCompilationDuration();
+					}
+				}
+			}
+		}
+	}
+
+	// TODO: Testing duration sums only!
+	private long _durations = 0;
+	private long _durationsStrict = 0;
+
+	// TODO: Testing duration sums only!
+	@Override
+	public List<ITopListScore> buildTopList() {
+		_durations = 0;
+		_durationsStrict = 0;
+		walkTree(model);
+		System.out.println("TOTAL Durations: " + _durations);
+		System.out.println("TOTAL Durations strict: " + _durationsStrict);
+
+		return super.buildTopList();
 	}
 }
